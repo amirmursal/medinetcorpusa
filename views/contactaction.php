90d48282c2bd4b64a466d4b878a513de0d9fc918
amursal <amursal@thinkanalytics.com>
2017-08-21 18:23:06 +0530

    added view folder - changes for index.html completed

---
+++ views/contactaction.php
@@ -0,0 +1,25 @@
+<?php  ?><?php
+require_once ("Email_class.php");
+$firstname=$_POST['first_name'];
+$email=$_POST['email'];
+$msg = $_POST['message'];
+{
+	$Sender="donotreply@medicorpnet.com";
+	$Recipiant="medinetcorpllc@gmail.com";
+	$Bcc = "";
+	$Subject="www.medicorpusa.com - contactus details";
+	$message.=" \n";
+	$message.="Below are the details: \n";
+	$message.=" \n";
+	$message.="First Name: $firstname\n";
+	$message.="Email: $email\n";	
+	$message.="Message: $$msg\n";
+	$msg = new Email($Recipiant, $Sender, $Subject);
+	$msg->Bcc = $Bcc;
+	$msg->TextOnly = true;
+	$msg->Content = $message;
+	$SendSuccess = $msg->Send();
+	header("Location: Thanks.htm");
+}
+?>
+<? ob_flush(); ?>
